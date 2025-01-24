22ff23f512e52972c9e3387a05c20de76ee0165d
Satya Suranjeet Jena <102950648+Satyasuranjeet@users.noreply.github.com>
2025-01-24 22:25:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -342,4 +342,5 @@ def disable_user():
     return jsonify({"message": "User API access disabled"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', False))
+    port = int(os.getenv("PORT", 5000))  # Render assigns a dynamic port
+    app.run(host='0.0.0.0', port=port, debug=os.getenv('FLASK_DEBUG', False))
